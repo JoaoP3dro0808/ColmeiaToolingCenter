036b5dda9c0f4be5cb39272d54cc92944f8d8b3f
Klippel <veigarsemr@gmail.com>
2026-01-23 11:50:38 -0300

    Analytics import fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { routes } from './routes/Routes'
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={routes}/>
+    <Analytics />
   </StrictMode>,
-  <Analytics />
 )

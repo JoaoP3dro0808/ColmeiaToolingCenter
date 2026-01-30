61cd2cbecce279343c8c17b618a00d1b0c77dfb6
Klippel <veigarsemr@gmail.com>
2026-01-30 14:40:52 -0300

    New links added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,9 @@ export default function Home() {
     { title: 'Turnover Health', filePath: 'S:/TEC/Technical_Function/1.1.Process_Develop_Tools/03.Shared/01.Process_Development/GD/02_Atividades em andamento/João Klippel/TurnoverHealth.pbix', relevance: 2 },
     { title: 'Calendário', link: '/calendar', relevance: 5 },
     { title: 'Dashboard - Dias de Afiação', filePath: 'S:/TEC/Technical_Function/1.1.Process_Develop_Tools/03.Shared/02.Tooling_Center/01.Logistica/3. Ferramentas para Afiação/Lista Lotes de Afiação/Desenvolvimento/Dashboard - CargaMáquina.lnk', relevance: 3 },
-];
+    { title: 'Reclamações de Qualidade', link: 'https://app.powerbi.com/groups/26c107da-94ff-4b8f-9de4-1da4f23fea94/reports/00e28cfd-7b16-4b86-9c42-ffc3a8a6e6d6/ReportSection?experience=power-bi', relevance: 2 },
+    { title: 'Desempenho de Ferramentas', link: 'https://app.powerbi.com/groups/26c107da-94ff-4b8f-9de4-1da4f23fea94/reports/f31b6875-7775-47a0-af45-43b3b9ad33b2/ReportSection?experience=power-bi', relevance: 1 }
+  ];
   
   return <Colmeia links={meusLinks} />;
 }

bc4b3f2182b3efb4516c247b1bd5a27aa7fcd92d
JoaoP3dro0808 <veigarsemr@gmail.com>
2026-04-07 09:13:53 -0300

    Consumption projection - Silver electrode link added

--- src/pages/SilverElectrode.jsx
+++ src/pages/SilverElectrode.jsx
@@ -9,6 +9,7 @@ export default function SilverElectrode() {
     { title: 'Estoque', link: 'https://app.powerbi.com/groups/26c107da-94ff-4b8f-9de4-1da4f23fea94/reports/5200b17f-82dc-4405-bfe5-cd2d540caf8f/3394c9f36908ae0212b0?experience=power-bi', relevance: 1 },
     { title: 'Acompanhamento Mensal', link: 'https://app.powerbi.com/groups/26c107da-94ff-4b8f-9de4-1da4f23fea94/reports/a2602571-dc69-4748-95f1-c8e3108404b9/796010f0104d02cb1db4?experience=power-bi', relevance: 1, color: { from: '#ff7e7eff', to: '#a7021bff' } },
     { title: 'Refugos', link: 'https://app.powerbi.com/groups/26c107da-94ff-4b8f-9de4-1da4f23fea94/reports/0b9c253c-05cb-4a89-bbaf-15794511e7e0/78df1f37e9c3352a7072?experience=power-bi', relevance: 1 },
+    { title: 'Previsão de cosnumo', link: 'https://app.powerbi.com/groups/26c107da-94ff-4b8f-9de4-1da4f23fea94/reports/2683a480-7446-4223-bcdf-44631fd9ec34/078bbcf405002d835c20?experience=power-bi', relevance: 1 },
   ];
   
   return <Colmeia links={meusLinks} />;

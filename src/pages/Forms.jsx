d36a33eab576026e645ef9b7be237a5480612878
João Klippel <joao.klippel@br.bosch.com>
2026-08-12 13:38:09 -0300

    Link para formulário de suporte 5s adicionado

--- src/pages/Forms.jsx
+++ src/pages/Forms.jsx
@@ -10,6 +10,7 @@ export default function Forms() {
     { title: 'Mod. AM - Fabricação', link: 'https://forms.cloud.microsoft/e/VtcyZkARf2?embed=true', relevance: 1 },
     { title: 'Mod. AM - Preset', link: 'https://forms.cloud.microsoft/e/ZDLhE0LqAu?embed=true', relevance: 1 },
     { title: 'Modificação de Desenho', link: 'https://forms.cloud.microsoft/e/RJZD1WTRSN?embed=true', relevance: 1 },
+    { title: 'Requisição de suporte 5s', link: 'https://forms.cloud.microsoft/e/NLvEQSUHet?embed=true', relevance: 1, color: { from: 'rgb(255, 72, 81)', to: 'rgb(143, 40, 49)' } },
   ];
   
   return <Colmeia links={meusLinks} />;
